0e531e4b3b8a8efd3d4af6e03bd0df3c1babc1bc
Zhixing Meng <z3meng@ucsd.edu>
2015-10-17 20:30:27 -0700

    finally finished display db

--- app.js
+++ app.js
@@ -68,13 +68,14 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 
   console.log('Connected to postgres! Getting schemas...');
 
+  /*
   client.query('SELECT * FROM test_table', function(err, result) {
     done();
     if (err)
       { console.error(err); response.send("Error " + err); }
     else
       { response.send(result.rows); }
-  });
+  }); */
 }); 
 
 
